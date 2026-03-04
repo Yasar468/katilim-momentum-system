a9538ff42a5c0230770d2bb0a4d7944014dcf328
Yasar468 <yasar.ardic.468@gmail.com>
2026-03-05 00:50:25 +0200

    github backup fix

--- github_backup.py
+++ github_backup.py
@@ -7,9 +7,6 @@ REPO = "Yasar468/katilim-momentum-system"
 
 def upload_file(path):
 
-    with open(path, "rb") as f:
-        content = base64.b64encode(f.read()).decode()
-
     filename = os.path.basename(path)
 
     url = f"https://api.github.com/repos/{REPO}/contents/{filename}"
@@ -18,9 +15,22 @@ def upload_file(path):
         "Authorization": f"token {GITHUB_TOKEN}"
     }
 
+    # önce dosyanın sha bilgisini al
+    r = requests.get(url, headers=headers)
+
+    sha = None
+    if r.status_code == 200:
+        sha = r.json()["sha"]
+
+    with open(path, "rb") as f:
+        content = base64.b64encode(f.read()).decode()
+
     data = {
         "message": f"auto update {filename}",
         "content": content
     }
+
+    if sha:
+        data["sha"] = sha
 
     requests.put(url, json=data, headers=headers)
